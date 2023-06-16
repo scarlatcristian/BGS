79c9bfea75707908d5e6cb9c3bd2df6940ca5494
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-17 00:36:25 +0300

    added btn animation and text

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const btn = document.querySelector(".btn");
+const btnBorder = document.querySelector(".rotate-container");
+const text = document.querySelector(".text-about");
+const title = document.querySelectorAll(".title");
+
+let counter = 0;
+let timer = 2000;
+let intervalId;
+
+const addClassInterval = () => {
+  title.forEach((t) => {
+    intervalId = setInterval(() => {
+      if (counter >= 4) {
+        clearInterval(intervalId);
+        return;
+      }
+      t.classList.add("color-change");
+      counter++;
+    }, timer);
+    timer += 1000;
+  });
+};
+
+addClassInterval();
+
+btn.addEventListener("click", () => {
+  btn.classList.add("scale-down");
+  btnBorder.classList.add("scale-down");
+  setTimeout(() => {
+    btnBorder.style.border = "none";
+    btn.remove();
+    btnBorder.remove();
+  }, 400);
+
+  setTimeout(() => {
+    text.classList.add("transition");
+  }, 500);
+
+  intervalId();
+});

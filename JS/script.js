590180210b80b54c305bea11816ca52eae5ec7d7
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-17 07:48:48 +0300

    added cursor effect

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const btnStart = document.querySelector(".btn-start-animation");
+const btnBorder = document.querySelector(".rotate-container");
+const text = document.querySelector(".text-about");
+const title = document.querySelectorAll(".title");
+const particles = document.getElementById("particles-js");
+
+const btnArrow = document.querySelector(".arrow");
+
+let counter = 0;
+let timer = 2000;
+let intervalId;
+
+const addClassInterval = () => {
+  title.forEach((t) => {
+    intervalId = setInterval(() => {
+      if (counter >= 4) {
+        clearInterval(intervalId);
+        return;
+      }
+      t.classList.add("color-change");
+      counter++;
+    }, timer);
+    timer += 1000;
+  });
+};
+
+// After first btn click
+btnStart.addEventListener("click", () => {
+  // Initialize the particles
+  particlesJS.load("particles-js", "particlesjs-config.json");
+  particles.style.opacity = 1;
+
+  btnStart.classList.add("scale-down");
+  btnBorder.classList.add("scale-down");
+  setTimeout(() => {
+    btnBorder.style.border = "none";
+    btnStart.remove();
+    btnBorder.remove();
+  }, 400);
+
+  setTimeout(() => {
+    text.classList.add("transition");
+  }, 500);
+
+  const addClassInterval = () => {
+    title.forEach((t) => {
+      intervalId = setInterval(() => {
+        if (counter >= 4) {
+          clearInterval(intervalId);
+          return;
+        }
+        t.classList.add("color-change");
+        counter++;
+      }, timer);
+      timer += 1000;
+    });
+  };
+
+  addClassInterval();
+});

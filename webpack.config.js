366440e52d869f340e730eccb8883fa4903c06b1
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-27 16:39:31 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: "./src/script.js",
   output: {
     filename: "./src/bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "BGS"),
   },
 };
